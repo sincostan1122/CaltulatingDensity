ee1e7e7e70c5606e7365088f5153a0bbca9b4c79
sincostan1122 <youngam1122s@gmail.com>
2022-05-07 02:09:19 +0900

    update v0.2

--- mainProjrct/mainProjrct/main.c
+++ mainProjrct/mainProjrct/main.c
@@ -6,9 +6,14 @@
 #include <Windows.h>
 void intro();
 void gotoxy(int x, int y);
-void start_select();
+int start_select();
+void option(float x, float y);
+
+float range1 = 0.5;
+float range2 = 1.5;
+
 int main() {
-	intro();
+
 
 	//기본 변수 선언
 	FILE* fp;
@@ -16,23 +21,58 @@ int main() {
 	char density[100];
 	char name[200];
 	int d = 0;
-	//반복될 함수
-	while (1) {
-		start_select();
-		scanf("%d", d);
-	}
+
 	fp = fopen("nameData.txt", "r");
 	while (fgets(num, sizeof(num), fp) != NULL) {
 		fgets(density, sizeof(density), fp);
 		fgets(name, sizeof(name), fp);
-		name[strlen(name) - 1] = NULL;
-		d = atoi(density);
-		if (d > 120) {
-			printf("%s, %d\n", name, d);
+		d++;
+	}
+	fclose(fp);
+
+	intro();
+	//반복될 함수
+	while (1) {
+		int y = start_select();
+		if (y == 2) {
+			system("cls");
+			fopen("nameData.txt", "r");
+			while (fgets(num, sizeof(num), fp) != NULL) {
+				num[strlen(num) - 1] = NULL;
+				fgets(density, sizeof(density), fp);
+				density[strlen(density) - 1] = NULL;
+				fgets(name, sizeof(name), fp);
+				name[strlen(name) - 1] = NULL;
+				printf("%s - %s %sg/mL\n\n", num, name, density);
+			}
+			printf("아무 키나 눌러 뒤로가기");
+			fclose(fp);
+			char p = _getch();
+		}
+		else if (y == 3) { // 데이터 추가
+			system("cls");
+			printf("추가할 데이터의 이름과 밀도를 입력하세요.");
+			fp = fopen("nameData.txt", "a");
+			scanf("%s %s", name, density);
+			d++;
+			
+			fputs(_itoa(d, num, 10), fp);
+			fputs("\n", fp);
+			fputs(density, fp);
+			fputs("\n", fp);
+			fputs(name, fp);
+			fputs("\n", fp);
+			fclose(fp);
+		}
+		else if (y == 4) {//오차범위 재설정
+			option(range1, range2);
+		}
+		else if (y == 5) { //종료
+			system("cls");
+			return 0;
 		}
 	}
-
-	fclose(fp);
+	
 	return 0;
 
 }
@@ -49,13 +89,14 @@ void gotoxy(int x, int y) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void start_select() {
+int start_select() {  //메인 인터페이스
 	system("cls");
 	int s = 1;
 	char select = NULL;
 	while (1) {
 		system("cls");
-		printf("밀도 계산\n데이터 추가\n설정\n종료");
+		printf("밀도 계산\n데이터 열람\n데이터 추가\n오차범위 재설정\n종료");
+
 		if (s == 1) {
 			gotoxy(10, 0);
 		}
@@ -63,42 +104,156 @@ void start_select() {
 			gotoxy(12, 1);
 		}
 		else if (s == 3) {
-			gotoxy(5, 2);
+			gotoxy(12, 2);
 		}
 		else if (s == 4) {
-			gotoxy(5, 3);
+			gotoxy(16, 3);
+		}
+		else if (s == 5) {
+			gotoxy(5, 4);
 		}
 		printf("<");
-		while (1) {
-			
-			if (_kbhit()) {
-				
+
+		while (1) {		
+			if (_kbhit()) {				
 				select = _getch();
 				if (select == -32) {
 					select = _getch();
-
-					switch (select) {
-						
+					switch (select) {						
 						case 72:
-
 							if (s != 1) {
 								s--;
 								break;
 							}
 						case 80:
-
-							if (s != 4) {
+							if (s != 5) {
 								s++;
-
 								break;
 							}
 					}
 					break;
 				}
+				else if (select == 'a') {
+					return s;
+				}
 			}
-
-
-
 		}
+	}
+}
+
+void option() { //오차범위 설정
+	char select = NULL;
+	int s = 1;
+	while (1) {
+		system("cls");
+		printf("1차 오차범위 : %g\n2차 오차범위 : %g\n\n", range1, range2);
+		printf("1차 오차범위 수정\n2차 오차범위 수정\n뒤로가기");
+		if (s == 1) {
+			gotoxy(18, 3);
+		}
+		else if (s == 2) {
+			gotoxy(18, 4);
+		}
+		else if (s == 3) {
+			gotoxy(10, 5);
+		}
+		printf("<");
+		int x = 0;
+		while (1) {
+			if (_kbhit()) {
+				select = _getch();
+				if (select == -32) {
+					select = _getch();
+					switch (select) {
+					case 72:
+						if (s != 1) {
+							s--;
+							break;
+						}
+					case 80:
+						if (s != 3) {
+							s++;
+
+							break;
+						}
+					}
+					break;
+				}
+				else if (select == 'a') {
+					x = s;
+					break;
+				}
+			}
+		}
+		if (x == 1) {			
+			float q = 0.0;
+			int error = 0;
+			while (1) {
+				system("cls");
+				printf("현재 1차 오차범위 : %g\n", range1);
+				printf("변경할 값을 입력해주세요. : ");
+				if (error == 1) {
+					gotoxy(0, 3);
+					printf("1차 오차범위는 2차 오차범위보다 클 수 없습니다.\n현재 2차 오차범위:%g", range2);
+					gotoxy(28, 1);
+					error = 0;
+				}
+				else if (error == 2) {
+					gotoxy(0, 3);
+					printf("오차범위는 0보다 작을 수 없습니다.");
+					gotoxy(28, 1);
+					error = 0;
+				}
+				scanf("%f", &q);
+				if (q > range2) {
+					error = 1;
+				}
+				else if (q < 0) {
+					error = 2;
+				}
+				else {
+					break;
+				}
+			}
+			range1 = q;
+			x = 0;
+		}
+		else if (x == 2) {
+			float q = 0.0;
+			int error = 0;
+			while (1) {
+				system("cls");
+				printf("현재 2차 오차범위 : %g\n", range2);
+				printf("변경할 값을 입력해주세요. : ");
+				if (error == 1) {
+					gotoxy(0, 3);
+					printf("2차 오차범위는 1차 오차범위보다 작을 수 없습니다.\n현재 1차 오차범위:%g", range1);
+					gotoxy(28, 1);
+					error = 0;
+				}
+				else if (error == 2) {
+					gotoxy(0, 3);
+					printf("오차범위는 0보다 작을 수 없습니다.");
+					gotoxy(28, 1);
+					error = 0;
+				}
+				scanf("%f", &q);
+				if (q < range1) {
+					error = 1;
+				}
+				else if (q < 0) {
+					error = 2;
+				}
+				else {
+					break;
+				}
+			}
+			range2 = q;
+			x = 0;
+		}
+		else if (x == 3) {
+			break;
+		}
+
 	}
 }
